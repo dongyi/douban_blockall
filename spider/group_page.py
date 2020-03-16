b35efa88b9fcd0af90e24450fde827af7b8c4d78
dongyi <juvenpp@gmail.com>
2020-03-16 11:18:53 +0800

    next page and member detail fetch

--- spider/group_page.py
+++ spider/group_page.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-# Created on 2020-03-16 10:55:47
-# Project: douban_group
+# Created on 2020-03-16 11:00:06
+# Project: douban
 
 from pyspider.libs.base_handler import *
 
@@ -16,9 +16,17 @@ class Handler(BaseHandler):
 
     @config(age=10 * 24 * 60 * 60)
     def index_page(self, response):
+        next_page_urls = []
         for each in response.doc('a[href^="http"]').items():
-            self.crawl(each.attr.href, callback=self.detail_page)
+            if 'members?start=' in each.attr.href:
+                next_page_urls.append(each.attr.href)
+            if '/people/' in each.attr.href:
+                self.crawl(each.attr.href, callback=self.detail_page)
 
+        next_page_start = min([int(i.split('?start=')[-1]) for i in next_page_urls])
+        next_page_url = 'https://www.douban.com/group/685388/members?start=' + str(next_page_start)
+        self.crawl(next_page_url, callback=self.index_page)
+           
     @config(priority=2)
     def detail_page(self, response):
         return {
